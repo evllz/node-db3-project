9ee437127a54634d12d280f786f3ad546a3dc9a1
evllz <erickvillegsaecheverria@gmail.com>
2021-02-02 21:24:56 -0800

    MVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,44 @@
 // scheme-model
+const db = require('../../data/db-config');
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove,
+};
+
+async function find() {
+	return await db('schemes');
+}
+
+async function findById(id) {
+	return await db('schemes').where({ id }).first();
+}
+
+async function findSteps(id) {
+	return await db('schemes')
+		.join('steps', 'schemes.id', '=', 'steps.scheme_id')
+		.where({ 'schemes.id': id })
+		.select(
+			'schemes.id',
+			'schemes.scheme_name',
+			'steps.step_number',
+			'steps.instructions',
+		);
+}
+
+async function add(scheme) {
+	const ids = db('schemes').insert(scheme);
+	return await findById(ids[0]);
+}
+
+async function update(changes, id) {
+	return await db('schemes').where({ id }).update(changes);
+}
+
+async function remove(id) {
+	return await db('schemes').where({ id }).del();
+}
